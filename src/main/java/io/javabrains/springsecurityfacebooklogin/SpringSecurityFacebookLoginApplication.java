c72f6dc7da70d5e20f1ddd5af464d48cef8f12f6
maximebf <84409780+maxiPool@users.noreply.github.com>
2021-10-18 14:49:47 -0400

    initial commit

---
+++ src/main/java/io/javabrains/springsecurityfacebooklogin/SpringSecurityFacebookLoginApplication.java
@@ -0,0 +1,14 @@
+package io.javabrains.springsecurityfacebooklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+public class SpringSecurityFacebookLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityFacebookLoginApplication.class, args);
+    }
+
+}
